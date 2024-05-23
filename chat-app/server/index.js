9c48ca7ce39605acd29a82e1e3504f64ae22eaa0
OFFICIALNITIN <official.nitin105@gmail.com>
2024-05-23 18:05:04 +0530

    made some changes in server

--- chat-app/server/index.js
+++ chat-app/server/index.js
@@ -12,7 +12,7 @@ const router = require("./router");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+  cors: { origin: "https://devchatapp.vercel.app/", methods: ["GET", "POST"] },
 });
 
 app.use(cors());
